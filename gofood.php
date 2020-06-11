c1fd971f12655999fa2c33ebaa11f439e9d24ed7
gumaygo <55977935+gumaygo@users.noreply.github.com>
2020-06-11 22:10:04 +0700

    Update gofood.php

--- gofood.php
+++ gofood.php
@@ -2,14 +2,14 @@
 include 'curl.php';
 function headers($token = null) {
 	$huruf = '0123456789ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    	$uniq = '';
-    	for ($i = 0; $i < 16; $i++) {
-        $uniq .= $huruf[mt_rand(0, strlen($huruf) - 1)];
-    	}
+   $uniq = '';
+   for ($i = 0; $i < 16; $i++) {
+    $uniq .= $huruf[mt_rand(0, strlen($huruf) - 1)];
+}
 
-    if  ($token != "") {
+if  ($token != "") {
 
-        $headers = array(
+    $headers = array(
         'Accept: application/json',
         'X-Session-ID: 28667f0c-80e8-43db-9ab4-c6d411f00a86',
         'X-AppVersion: 3.40.0',
@@ -31,13 +31,13 @@ function headers($token = null) {
         'Content-Type: application/json; charset=UTF-8',
         'Host: api.gojekapi.com',
         'User-Agent: okhttp/3.12.1'
-        );
+    );
 
-        return $headers;
+    return $headers;
 
-    } else {
+} else {
 
-        $headers = array(
+    $headers = array(
         'Accept: application/json',
         'D1: 41:ED:AC:6E:29:0D:B2:24:C4:89:42:02:4D:C5:0F:33:72:DC:D1:9D:14:68:45:AD:84:9C:74:6E:3F:0E:8D:4C',
         'X-Session-ID: 28667f0c-80e8-43db-9ab4-c6d411f00a86',
@@ -57,72 +57,80 @@ function headers($token = null) {
         'Content-Type: application/json; charset=UTF-8',
         'Host: api.gojekapi.com',
         'User-Agent: okhttp/3.12.1'
-        );
+    );
 
-        return $headers; 
+    return $headers; 
 
-    }
+}
 
 }
 
 function register_gojek() {
-     $fakename = curl('https://fakenametool.net/random-name-generator/random/id_ID/indonesia/1');
-     preg_match('/<span>(.*?)<\/span>/s', $fakename, $name);
-     $email = strtolower(str_replace(' ',  '', $name[1])).rand(0000,1111).'@gmail.com';
+   $fakename = curl('https://fakenametool.net/random-name-generator/random/id_ID/indonesia/1');
+   preg_match('/<span>(.*?)<\/span>/s', $fakename, $name);
+   $email = strtolower(str_replace(' ',  '', $name[1])).rand(0000,1111).'@gmail.com';
 
-     echo "Nomor  : ";
-     $phone = trim(fgets(STDIN));
-     $register = curl('https://api.gojekapi.com/v5/customers', '{"email":"'.$email.'","name":"'.$name[1].'","phone":"+'.$phone.'","signed_up_country":"ID"}', headers());
+   echo "Nomor  : ";
+   $phone = trim(fgets(STDIN));
+   $register = curl('https://api.gojekapi.com/v5/customers', '{"email":"'.$email.'","name":"'.$name[1].'","phone":"+'.$phone.'","signed_up_country":"ID"}', headers());
 
-    if (stripos($register, '"success":true')) {
-        $otp_token = fetch_value($register,  '"otp_token":"', '"');
-        echo "OTP : ";
-        $otp_code = trim(fgets(STDIN));
+   if (stripos($register, '"success":true')) {
+    $otp_token = fetch_value($register,  '"otp_token":"', '"');
+    echo "OTP : ";
+    $otp_code = trim(fgets(STDIN));
 
-        $verify = curl('https://api.gojekapi.com/v5/customers/phone/verify', '{"client_name":"gojek:cons:android","client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e","data":{"otp":"'.$otp_code.'","otp_token":"'.$otp_token.'"}}', headers());
+    $verify = curl('https://api.gojekapi.com/v5/customers/phone/verify', '{"client_name":"gojek:cons:android","client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e","data":{"otp":"'.$otp_code.'","otp_token":"'.$otp_token.'"}}', headers());
 
-        if (stripos($verify, '"access_token"')) {
-            $access_token = fetch_value($verify, '"access_token":"','"');
-            
-                  $claim = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', '{"promo_code":"GOFOODBOBA07"}', headers($access_token));
-                  echo "\nAuthorization: Bearer : ".$access_token;
-                   $live = "token-accounts.txt";
-    $fopen1 = fopen($live, "a+");
-    $fwrite1 = fwrite($fopen1, "TOKEN => ".$access_token." \n NOMOR => ".$phone." \n");
-    fclose($fopen1);
-    					echo "\n";
-    echo "[+] File Token saved in ".$live." \n";
-                  echo "[+] Mencoba Redem Voucher GOFOODBOBA07\n";
-                        if (stripos($claim, '"success":true')) {
-                        echo "Berhasil Claim Kak? Selamat Makan\n";
-                    } else {
-                        echo "Cie Gagal Redem Ga Jadi Makan\n";
-                }
-                    echo "\n";
-                    sleep(5);
-                  $claim1 = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', '{"promo_code":"AYOCOBAGOJEK"}', headers($access_token));
-                  echo "[+] Mencoba Redem Voucher AYOCOBAGOJEK\n";
-                        if (stripos($claim1, '"success":true')) {
-                        echo "Berhasil Claim Kak? Selamat Jalan-Jalan\n";
-                    } else {
-                        echo "Cie Gagal Redem Ga Jadi Jalan-Jalan\n";
-                    }
-                    echo "\n";
-                    sleep(9);
-                  $claim2 = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', '{"promo_code":"COBAINGOJEK"}', headers($access_token));
-                  echo "[+] Mencoba Redem Voucher COBAINGOJEK\n";
-                    if (stripos($claim2, '"success":true')) {
-                        echo "Berhasil Claim Kak? Selamat Jalan-Jalan\n";
-                    } else {
-                        echo "Cie Gagal Redem Ga Jadi Makan\n";
-                    }
+    if (stripos($verify, '"access_token"')) {
+        $access_token = fetch_value($verify, '"access_token":"','"');
 
-            } else { 
-                echo "Promo tidak ditemukan\n";
-            }
-    } else {
-        echo "Gagal mendaftar No HP / Email Sudah Terdaftar\n";
+        $claim = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', '{"promo_code":"EATLAH"}', headers($access_token));
+        echo "\nAuthorization: Bearer : ".$access_token;
+        $live = "token-accounts.txt";
+        $fopen1 = fopen($live, "a+");
+        $fwrite1 = fwrite($fopen1, "TOKEN => ".$access_token." \n NOMOR => ".$phone." \n");
+        fclose($fopen1);
+        echo "\n";
+        echo "[+] File Token saved in ".$live." \n";
+        echo "[+] Mencoba Redem Voucher EATLAH\n";
+        if (stripos($claim, '"success":true')) {
+            echo "Berhasil Claim Kak? Selamat Makan\n";
+        } else {
+            echo "Cie Gagal Redem Ga Jadi Makan\n";
+        }
+        echo "\n";
+        sleep(5);
+        $claim1 = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', '{"promo_code":"COBAGOFOOD0906"}', headers($access_token));
+        echo "[+] Mencoba Redem Voucher COBAGOFOOD0906\n";
+        if (stripos($claim1, '"success":true')) {
+            echo "Berhasil Claim Kak? makan\n";
+        } else {
+            echo "Cie Gagal Redem Ga Jadi makan\n";
+        }
+        echo "\n";
+        sleep(9);
+        $claim2 = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', '{"promo_code":"PAKEGOFOOD0906"}', headers($access_token));
+        echo "[+] Mencoba Redem Voucher PAKEGOFOOD0906\n";
+        if (stripos($claim2, '"success":true')) {
+            echo "Berhasil Claim Kak? Selamat makan\n";
+        } else {
+            echo "Cie Gagal Redem Ga Jadi Makan\n";
+        }
+        sleep(9);
+        $claim2 = curl('https://api.gojekapi.com/go-promotions/v1/promotions/enrollments', '{"promo_code":"COBAGOFOOD0906"}', headers($access_token));
+        echo "[+] Mencoba Redem Voucher COBAGOFOOD0906\n";
+        if (stripos($claim2, '"success":true')) {
+            echo "Berhasil Claim Kak? Selamat makan\n";
+        } else {
+            echo "Cie Gagal Redem Ga Jadi Makan\n";
+        }
+
+    } else { 
+        echo "Promo tidak ditemukan\n";
     }
+} else {
+    echo "Gagal mendaftar No HP / Email Sudah Terdaftar\n";
+}
 
 }
 echo "Gojexnxx Tools - Gojek Register + Claim GoFood\n";
